370f57de8ce590ef004332f74699426d53cb9350
Alfredo <puertorocat@gmail.com>
2019-05-20 20:47:00 -0400

    changes to app.js

--- mini-ecommerce/src/App.js
+++ mini-ecommerce/src/App.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "./components/Navbar";
+import ProductList from "./components/ProductList";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import Default from "./components/Default";
 
-function App() {
-  return (
-  <div className = "container">
-  <div className = "row">
-  <div className= "col-6">column number one</div>
-  <div className= "col-6">
-  <span>
-    <i className="fas fa-home" />
-  </span>
-
-  </div>
-  </div>
-  </div>
-  );
+class App extends Component {
+  render() {
+    return (
+    <React.Fragment>
+      <Navbar></Navbar>
+      <ProductList></ProductList>
+      <Details></Details>
+      <Cart></Cart>
+      <Default></Default>
+    </React.Fragment>
+    )
+  }
 }
 
 export default App;
