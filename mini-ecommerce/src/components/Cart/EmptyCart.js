970fc343f7e08238b2b1664b7e8360a43fb3a82b
Alfredo <puertorocat@gmail.com>
2019-05-22 16:36:34 -0400

    new changes

---
+++ mini-ecommerce/src/components/Cart/EmptyCart.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function EmptyCart() {
+    return (
+    <div className="container mt-5">
+    <div className="row">
+    <div className="col-10 mx-auto text-align-center text-titile">
+    
+    <h1>your cart is currently empty</h1>
+    
+    
+    </div> 
+    </div>
+     </div>
+    )
+}
