167b950c0571bc427725d4947609ad9edefc1d05
Alfredo <puertorocat@gmail.com>
2019-05-21 21:22:56 -0400

    new changes

--- mini-ecommerce/src/components/Product.js
+++ mini-ecommerce/src/components/Product.js
@@ -21,6 +21,15 @@ export default class Product extends Component {
         in cart</p>):(<i className="fas fa-cart-plus"/>)}
        </button>
       </div>
+      {/* card footer */}
+      <div className="card-footer d-flex justify-content-between">
+        <p className="align-self-center mb-0">
+          {title}
+        </p>
+        <h5 className="text-blue font-italic mb-0">
+        <span className="mr-1">$</span>
+        </h5>
+      </div>
       </div>
 
 
